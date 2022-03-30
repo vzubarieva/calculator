6cc813dc4fdbc58f4081a8aa16411a6789418257
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-03-29 22:30:46 -0700

    Add tags and scripts file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+alert(add(number1,number2));
