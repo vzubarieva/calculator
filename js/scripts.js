7ca01a3b3a77e56cf0079a4614d3d85560f47784
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-03 17:13:59 -0700

    update scripts with user interface logic
    co-authored-by: Niles ONeil <nilesoneil@gmail.com>
    co-authored-by: Alexander Kirkpatrick <knibz@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,26 @@ function subtraction(number1, number2) {
 
 function multiply(number1, number2) {
   return number1 * number2;
-}
+};
 
 function divide(number1, number2) {
   return number1 / number2;
-}
+};
 
-const number1 = prompt("Enter a number:");
-const number2 = prompt("Enter another number:");
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
 
-alert(divide(number1,number2));
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+});
