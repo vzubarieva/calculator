064038e30bde90585d4349ba8e87cde3b0a393ae
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-06 19:19:52 -0700

    update interface logic and html
    co-authored-by:Sun Roberts <suerobertstx@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtraction(number1, number2) {
+function subtract(number1, number2) {
   return number1 - number2;
 }
 
@@ -15,19 +15,21 @@ function divide(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
